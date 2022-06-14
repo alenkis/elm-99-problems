c1ae1f5be1ce8ad195d80633de3443970f305cdd
Alen Kis <16632804+alenkis@users.noreply.github.com>
2022-06-14 22:18:41 +0200

    add solutions for first 3 List functions

--- src/Lists.elm
+++ src/Lists.elm
@@ -4,17 +4,33 @@ module Lists exposing (..)
 last : List a -> Maybe a
 last l =
     -- Write a function that returns the last element of a list
-    Debug.todo "not implemented"
+    l |> List.reverse |> List.head
 
 
 lastTwo : List a -> Maybe ( a, a )
 lastTwo l =
     -- Find the last but one (last and penultimate) elements of a list.
-    Debug.todo "not implemented"
+    case l of
+        [] ->
+            Nothing
+
+        [ _ ] ->
+            Nothing
+
+        [ x, y ] ->
+            Just ( x, y )
+
+        _ :: xs ->
+            lastTwo xs
 
 
 duplicate : List a -> List a
 duplicate l =
     -- Duplicate the elements of a list.
     -- e.g. [1,2,3] -> [1,1,2,2,3,3]
-    Debug.todo "not implemented"
+    case l of
+        [] ->
+            []
+
+        x :: xs ->
+            x :: x :: duplicate xs
