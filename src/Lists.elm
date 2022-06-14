91eab9068b2fe4c92eadb89a82313bfc98afb970
Alen Kis <16632804+alenkis@users.noreply.github.com>
2022-06-14 22:17:16 +0200

    initial commit

---
+++ src/Lists.elm
@@ -0,0 +1,20 @@
+module Lists exposing (..)
+
+
+last : List a -> Maybe a
+last l =
+    -- Write a function that returns the last element of a list
+    Debug.todo "not implemented"
+
+
+lastTwo : List a -> Maybe ( a, a )
+lastTwo l =
+    -- Find the last but one (last and penultimate) elements of a list.
+    Debug.todo "not implemented"
+
+
+duplicate : List a -> List a
+duplicate l =
+    -- Duplicate the elements of a list.
+    -- e.g. [1,2,3] -> [1,1,2,2,3,3]
+    Debug.todo "not implemented"
