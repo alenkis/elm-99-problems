b6454cdfaa483e2ed8297a930ba5f8ce82311dd4
Alen Kis <16632804+alenkis@users.noreply.github.com>
2022-06-15 14:54:33 +0200

    comment out 2nd and 3rd tests and add further instructions

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -18,20 +18,21 @@ all =
             , test "return nothing for empty list" <|
                 \_ -> Expect.equal Nothing (Lists.last [])
             ]
-        , describe "Lists.lastTwo"
-            [ test "returns Just value when list has more than 2 elements" <|
-                \_ -> Expect.equal (Just ( 4, 5 )) (Lists.lastTwo [ 1, 2, 3, 4, 5 ])
-            , test "returns Nothing when list has less than 2 elements" <|
-                \_ -> Expect.equal Nothing (Lists.lastTwo [ 1 ])
-            , test "returns Nothing for empty list" <|
-                \_ -> Expect.equal Nothing (Lists.lastTwo [])
-            ]
-        , describe "Lists.duplicate"
-            [ test "returns an empty list if given empty list" <|
-                \_ -> Expect.equal [] (Lists.duplicate [])
-            , test "returns a duplicated list of integers" <|
-                \_ -> Expect.equal [ 1, 1, 2, 2, 3, 3 ] (Lists.duplicate [ 1, 2, 3 ])
-            , test "returns a duplicated list of strings" <|
-                \_ -> Expect.equal [ "a", "a", "b", "b" ] (Lists.duplicate [ "a", "b" ])
-            ]
+
+        -- , describe "Lists.lastTwo"
+        --     [ test "returns Just value when list has more than 2 elements" <|
+        --         \_ -> Expect.equal (Just ( 4, 5 )) (Lists.lastTwo [ 1, 2, 3, 4, 5 ])
+        --     , test "returns Nothing when list has less than 2 elements" <|
+        --         \_ -> Expect.equal Nothing (Lists.lastTwo [ 1 ])
+        --     , test "returns Nothing for empty list" <|
+        --         \_ -> Expect.equal Nothing (Lists.lastTwo [])
+        --     ]
+        -- , describe "Lists.duplicate"
+        --     [ test "returns an empty list if given empty list" <|
+        --         \_ -> Expect.equal [] (Lists.duplicate [])
+        --     , test "returns a duplicated list of integers" <|
+        --         \_ -> Expect.equal [ 1, 1, 2, 2, 3, 3 ] (Lists.duplicate [ 1, 2, 3 ])
+        --     , test "returns a duplicated list of strings" <|
+        --         \_ -> Expect.equal [ "a", "a", "b", "b" ] (Lists.duplicate [ "a", "b" ])
+        --     ]
         ]
